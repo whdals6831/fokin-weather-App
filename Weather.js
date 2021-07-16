342b4d18669b52dc8de65a6394f689392fa049b7
whdals6831 <whdals6831@gmail.com>
2021-07-16 23:36:36 +0900

    done

--- Weather.js
+++ Weather.js
@@ -7,31 +7,43 @@ import { LinearGradient } from 'expo-linear-gradient';
 const weatherOptions = {
     Thunderstorm: {
         iconName: "weather-lightning",
-        gradient: []
+        gradient: ["#373B44", "#4286f4"],
+        title: "Thunderstorm in the house",
+        subTitle: "Actually, outside of the house"
     },
     Drizzle: {
-        iconName: "",
-        gradient: []
+        iconName: "weather-hail",
+        gradient: ["#89F7FE", "#66A6FF"],
+        title: "Drizzle",
+        subTitle: "Is like rain, but gay 🏳️‍🌈"
     },
     Rain: {
-        iconName: "",
-        gradient: []
+        iconName: "weather-rainy",
+        gradient: ["#00C6FB", "#005BEA"],
+        title: "Raining like a MF",
+        subTitle: "For more info look outside"
     },
     Snow: {
-        iconName: "",
-        gradient: []
+        iconName: "weather-snowy",
+        gradient: ["#7DE2FC", "#B9B6E5"],
+        title: "Cold as balls",
+        subTitle: "Do you want to build a snowman? Fuck no."
     },
     Atmosphere: {
-        iconName: "",
-        gradient: []
+        iconName: "weather-hail",
+        gradient: ["#89F7FE", "#66A6FF"]
     },
     Clear: {
-        iconName: "",
-        gradient: []
+        iconName: "weather-sunny",
+        gradient: ["#FF7300", "#FEF253"],
+        title: "Sunny as fuck",
+        subTitle: "Go get your ass burnt"
     },
     Clouds: {
-        iconName: "",
-        gradient: []
+        iconName: "weather-cloudy",
+        gradient: ["#D7D2CC", "#304352"],
+        title: "Clouds",
+        subTitle: "I know, fucking boring"
     },
 }
 
@@ -45,8 +57,9 @@ export default function Weather({temp, condition}) {
                 <MaterialCommunityIcons name={weatherOptions[condition].iconName} size={90} color="white" />
                 <Text style={styles.temp}>{temp}℃</Text>
             </View>
-            <View style={styles.halfContainer}>
-
+            <View style={{...styles.halfContainer, ...styles.textContainer}}>
+                <Text style={styles.title}>{weatherOptions[condition].title}</Text>
+                <Text style={styles.subTitle}>{weatherOptions[condition].subTitle}</Text>
             </View>
       </LinearGradient>
     );
@@ -71,5 +84,20 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    title: {
+        color: "white",
+        fontSize: 45,
+        fontWeight: "300",
+        marginBottom: 10,
+    },
+    subTitle: {
+        color: "white",
+        fontWeight: "600",
+        fontSize: 24
+    },
+    textContainer: {
+        paddingHorizontal: 20,
+        alignItems: "flex-start",
     }
 });
